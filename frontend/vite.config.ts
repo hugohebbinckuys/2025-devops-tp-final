b0a65f9526bfdfc006aa7ceb3250682030ae4013
hugohebbinckuys <hugo29.heb@gmail.com>
2025-12-12 00:20:21 +0100

    added dev stage to back dockerfile + dev target in the dockercompose [was prod] + backend proxy (dev)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -46,9 +46,12 @@ export default defineConfig({
     ],
   },
   server: {
+    host: '0.0.0.0',
+    port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://backend:8080',
+        //nom du service docker 
         changeOrigin: true,
       },
     },
